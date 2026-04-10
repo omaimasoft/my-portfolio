bd684e2be28a2d9394069a4fb6265a52e4467d3a
Omaima Boustik <omaimaboustik@gmail.com>
2026-04-10 10:58:41 +0200

    Update project

--- printed/views.py
+++ printed/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator  # ✅ مهم
+from django.core.paginator import Paginator
 from .models import PrintedProduct, PrintedCategory
 
 
@@ -7,7 +7,7 @@ def printed_list(request):
     products_qs = PrintedProduct.objects.select_related("category").all().order_by("-id")
     categories = PrintedCategory.objects.all()
 
-    paginator = Paginator(products_qs, 18)  # ✅ هنا العدد اللي بغيتي (10)
+    paginator = Paginator(products_qs, 16)
     page_number = request.GET.get("page")
     products = paginator.get_page(page_number)
 
@@ -19,5 +19,24 @@ def printed_list(request):
 
 
 def printed_detail(request, slug):
-    product = get_object_or_404(PrintedProduct, slug=slug)
-    return render(request, "printed/printed_detail.html", {"product": product})
+    product = get_object_or_404(
+        PrintedProduct.objects.select_related("category"),
+        slug=slug
+    )
+
+    # منتجات مشابهة من نفس التصنيف
+    related_products = PrintedProduct.objects.filter(
+        category=product.category
+    ).exclude(id=product.id).order_by("-id")[:4]
+
+    # مؤقتًا: آخر المنتجات
+    # إلى كان عندك sold_count بدليه بـ order_by("-sold_count")
+    best_sellers = PrintedProduct.objects.exclude(
+        id=product.id
+    ).order_by("-id")[:4]
+
+    return render(request, "printed/printed_detail.html", {
+        "product": product,
+        "related_products": related_products,
+        "best_sellers": best_sellers,
+    })
